d926ee54a8e81079572ef21fdfcafd800d5035a8
SASCHADDD <saschadanu23@gmail.com>
2026-05-04 02:13:34 +0700

    fix: correct comment syntax in Category and Product models

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,8 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+
 /**
- * membuat model category yang memiliki relasi one to many 
+ * membuat model category yang memiliki relasi one to many
  * dengan product untuk menyimpan data kategori produk karena setiap produk memiliki satu kategori,
  * sedangkan satu kategori dapat memiliki banyak produk
  */
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    #menambahkan kolom category_id dan menghubungkan dengan tabel category dengan foreign key di tabel products
+    // menambahkan kolom category_id dan menghubungkan dengan tabel category dengan foreign key di tabel products
     protected $fillable = ['user_id', 'category_id', 'name', 'qty', 'price'];
 
     public function user()
