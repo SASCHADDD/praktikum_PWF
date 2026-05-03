397923f87452cb90f08f45264dc33c2a60ef2dfe
SASCHADDD <saschadanu23@gmail.com>
2026-05-04 02:13:47 +0700

    feat: add product export route with authorization middleware

--- routes/web.php
+++ routes/web.php
@@ -32,6 +32,7 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/product/{product}/edit', [ProductController::class, 'edit'])->name('product.edit');
     Route::put('/product/{product}', [ProductController::class, 'update'])->name('product.update');
     Route::delete('/product/{id}', [ProductController::class, 'delete'])->name('product.delete');
+    Route::get('/product-export', [ProductController::class, 'export'])->name('product.export')->middleware('can:export-product');
 });
 
 require __DIR__.'/auth.php';
